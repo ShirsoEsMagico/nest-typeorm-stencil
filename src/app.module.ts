93f0255503140f9bb867cfb8913489147e642071
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2024-02-29 14:57:08 +0530

    changes-in database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,31 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
-import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
+import { User } from './user/entities/user.entity';
+import dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), DatabaseModule, UserModule, LoggerModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'oracle',
+      host: process.env.DATABASE_HOST, //Database host
+      port: Number(process.env.DATABASE_PORT), //Database port
+      username: process.env.DATABASE_USERNAME, //Database username
+      password: process.env.DATABASE_PASSWORD, //Database password
+      database: 'oracle-db', //Database name
+      sid: 'XE', //SID(Service Identifier)
+      entities: [User],
+      synchronize: true,
+      logging: true, //SQL Query Logging(for debugging)
+    }),
+    TypeOrmModule.forFeature([User]),
+    UserModule,
+    LoggerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
